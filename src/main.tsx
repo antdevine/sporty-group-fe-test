498f3a73bc7bb2e8caa333a8bf15d9fcd0c6198e
Anthony Devine <antdevine@hotmail.co.uk>
2025-05-26 19:28:56 +0100

    Removed tanstack dev tools
    * Removed the tanstack dev tools as I no longer need them since the API requests work as expected

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import './index.css'
 import App from './App.tsx'
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './lib/queryClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
     <App />
-    <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
